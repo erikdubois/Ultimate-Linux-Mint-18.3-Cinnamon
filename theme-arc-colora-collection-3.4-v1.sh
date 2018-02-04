df4ac1b755785cb4a2a9fba8900742afefe1c7eb
Erik Dubois <erik.dubois@gmail.com>
2018-02-04 16:33:39 +0100

    Comment : update arc theme on Sun Feb  4 16:33:39 CET 2018

--- theme-arc-colora-collection-3.4-v1.sh
+++ theme-arc-colora-collection-3.4-v1.sh
@@ -61,7 +61,7 @@
 rm -rf /tmp/Arc-Theme-Colora-Collection
 git clone https://github.com/erikdubois/Arc-Theme-Colora-Collection /tmp/Arc-Theme-Colora-Collection
 find /tmp/Arc-Theme-Colora-Collection -maxdepth 1 -type f -exec rm -rf '{}' \;
-cp -r /tmp/Arc-Theme-Colora-Collection/Cinnamon\ 3.6/* ~/.themes/
+cp -r /tmp/Arc-Theme-Colora-Collection/Cinnamon\ 3.4/* ~/.themes/
 rm -rf /tmp/Arc-Theme-Colora-Collection
 
 
